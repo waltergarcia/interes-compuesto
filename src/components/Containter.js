df647f7fec68c84be50a714644f07f086b08aae9
waltergarcia <waltez913@gmail.com>
2022-09-03 12:17:03 -0600

    Added logic to calculate compound interest and added configurations for deployment on GitHub Pages

---
+++ src/components/Containter.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Containter = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
+
+export default Containter
